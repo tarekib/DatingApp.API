78181820d2917c68ba8713e54d44eb9d69e8e99a
Tarek Ibrahim <tarek.ibrahim@ecomz.com>
2020-02-23 05:07:12 -0800

    Global Exception Handling

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -70,7 +70,7 @@ namespace DatingApp.API.Controllers
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.
-                GetBytes(_config.GetSection("AppSettings: Token").Value));
+                GetBytes(_config.GetSection("AppSettings:Token").Value));
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
             var tokenDescriptor = new SecurityTokenDescriptor
